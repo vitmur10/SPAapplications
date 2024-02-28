8a04678c2bffcb4bcfd3041c2610d16c0e0cf421
Vitaliy Murovaniy <95306614+vitmur10@users.noreply.github.com>
2024-02-28 03:00:03 +0200

    v1

--- SPAapplications/Сomment/urls.py
+++ SPAapplications/Сomment/urls.py
@@ -1,14 +1,16 @@
 from django.urls import path, include
 from . import views
-
+from django.conf import settings
+from django.conf.urls.static import static
 
 app_name = 'Comments'
 
 urlpatterns = [
-    path('', views.PostListView.as_view(), name='post'),
-    path('<int:post_id>/', views.PostDetailView.as_view(), name='post_detail'),
-    path('captcha/', include('captcha.urls')),
-    path('get_captcha/', views.get_captcha, name='get_captcha'),
-    path('api/v1/comments/<int:year>/<int:month>/<int:day>/<int:post_id>/', views.CommentAddView.as_view(), name='comment-list'),
-    path('api/v1/comments/<int:year>/<int:month>/<int:day>/<int:post_id>/create/', views.CommentAddView.as_view(), name='create-comment'),
-]
+                  path('', views.PostListView.as_view(), name='post'),
+                  path('<int:post_id>/', views.PostDetailView.as_view(), name='post_detail'),
+                  path('captcha/', include('captcha.urls')),
+                  path('get_captcha/', views.get_captcha, name='get_captcha'),
+                  path('api/v1/comments/<int:post_id>/', views.CommentAddView.as_view(), name='comment-list'),
+                  path('api/v1/comments<int:post_id>/create/', views.CommentAddView.as_view(), name='create-comment'),
+              ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT) + static(settings.MEDIA_URL,
+                                                                                           document_root=settings.MEDIA_ROOT)
